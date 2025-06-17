15847d743846bf295e683c9ff3debfb6fa3979ce
Hemang Mishra <hemangmishranew@gmail.com>
2025-06-17 12:29:18 +0530

    updated user table

--- src/main/kotlin/org/jagrati/jagratibackend/entities/User.kt
+++ src/main/kotlin/org/jagrati/jagratibackend/entities/User.kt
@@ -2,7 +2,9 @@ package org.jagrati.jagratibackend.entities
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
@@ -14,15 +16,31 @@ data class User(
     @Id
     @Column(name = "pid", length = 50)
     val pid: String,
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    val firstName: String,
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    val lastName: String,
+
+    @Column(name = "email", nullable = false, unique = true, length = 255)
     val email: String,
-    val hashedPassword: String
-): UserDetails{
+
+    @Column(name = "password_hash", nullable = false, length = 255)
+    val passwordHash: String,
+
+    @Column(name = "is_active", nullable = false)
+    var isActive: Boolean = true,
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    val userRoles: Set<UserRole> = emptySet()
+) : BaseEntity(),UserDetails{
     override fun getAuthorities(): Collection<GrantedAuthority?>? {
-        return listOf(SimpleGrantedAuthority(role.name))
+        return userRoles.map { SimpleGrantedAuthority(it.role.name) }
     }
 
     override fun getPassword(): String? {
-        return hashedPassword
+        return passwordHash
     }
 
     override fun getUsername(): String? {
