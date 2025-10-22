03c5ad9fb1316e26fc9fbba97a49e10266ae8c15
Hemang <hemangmishranew@gmail.com>
2025-10-22 18:24:33 +0530

    fixed timezone issue

--- src/main/kotlin/org/jagrati/jagratibackend/JagratiBackendApplication.kt
+++ src/main/kotlin/org/jagrati/jagratibackend/JagratiBackendApplication.kt
@@ -3,11 +3,14 @@ package org.jagrati.jagratibackend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.time.ZoneOffset
+import java.util.TimeZone
 
 @SpringBootApplication
 @EnableScheduling
 class JagratiBackendApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"))
     runApplication<JagratiBackendApplication>(*args)
 }
